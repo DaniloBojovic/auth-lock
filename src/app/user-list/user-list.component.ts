c2ac45aaf57ba1a7b86fda3f2731516f6f06b16f
Danilo <danilobojovicdb@gmail.com>
2024-03-22 14:59:23 +0100

    Add searchSubject

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, delay, finalize, of, tap } from 'rxjs';
+import {
+  Observable,
+  Subject,
+  debounceTime,
+  delay,
+  distinctUntilChanged,
+  finalize,
+  of,
+  tap,
+} from 'rxjs';
 import { UserService } from '../services/user.service';
 import { StateService } from '../services/state.service';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -20,6 +29,7 @@ export class UserListComponent implements OnInit {
   totalUsers = 100;
   currentSearchTerm = '';
   searchTerm = '';
+  private searchSubject = new Subject<string>();
 
   constructor(
     private userService: UserService,
@@ -29,30 +39,18 @@ export class UserListComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    debugger;
+    // This  sets up a subscription to the `searchSubject` observable.
+    // The `debounceTime(300)` operator ensures that events are processed at most once every 300ms.
+    // The `distinctUntilChanged()` operator ensures that the `loadUsers` method is only called when the search term changes.
+    // When a new value is emitted from `searchSubject` (which happens in the `onSearch` method => `this.searchSubject.next(this.currentSearchTerm)`,
+    // the `loadUsers` method is called with the new search term.
+    this.searchSubject
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((searchTerm) => {
+        this.loadUsers(1, 10, searchTerm);
+      });
     this.loadUsers(1, 10);
-
-    // this.userService
-    //   .getUsers()
-    //   .pipe(
-    //     delay(2000),
-    //     finalize(() => (this.loading = false))
-    //   )
-    //   .subscribe(
-    //     (users) => (this.users$ = of(users)),
-    //     (error: HttpErrorResponse) => {
-    //       console.error('An error occurred:', error);
-    //       if (error.status === 404) {
-    //         console.error('Not Found');
-    //       }
-    //     }
-    //   );
-    // this.users$ = this.userService.getUsers().pipe(
-    //   delay(2000),
-    //   finalize(() => (this.loading = false))
-    // );
-    // this.users$.subscribe((users) => {
-    //   console.log(users);
-    // });
   }
 
   pageEvent(event: PageEvent) {
@@ -64,7 +62,8 @@ export class UserListComponent implements OnInit {
     debugger;
     event.preventDefault();
     this.currentSearchTerm = this.searchTerm;
-    this.loadUsers(1, 10, this.currentSearchTerm);
+    //this.loadUsers(1, 10, this.currentSearchTerm);
+    this.searchSubject.next(this.currentSearchTerm);
   }
 
   loadUsers(page: number, pageSize: number, searchTerm?: string) {
@@ -72,7 +71,7 @@ export class UserListComponent implements OnInit {
     this.userService
       .getUsers(page, pageSize, searchTerm)
       .pipe(
-        delay(2000),
+        delay(1000),
         finalize(() => (this.loading = false)),
         tap((users) => console.log('USERS COMPONENT: ', users)),
       )
