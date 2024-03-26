19f1ada569f1911046b0dbcf6910090e07b0d789
Danilo <danilobojovicdb@gmail.com>
2024-03-26 11:17:24 +0100

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { RegisterComponent } from './user/register/register.component';
+import { RegisterComponent } from './user-auth/register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './user/login/login.component';
+import { LoginComponent } from './user-auth/login/login.component';
 import { AuthInterceptor } from './services/auth.interceptor';
-import { LogoutModule } from './user/logout/logout.module';
-import { AdminComponent } from './user/admin/admin.component';
-import { AdminFormComponent } from './user/admin/admin-form/admin-form.component';
+import { LogoutModule } from './user-auth/logout/logout.module';
+import { AdminComponent } from './user-auth/admin/admin.component';
+import { AdminFormComponent } from './user-auth/admin/admin-form/admin-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AddUserDialogComponent } from './dialogs/add-user-dialog/add-user-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserSearchComponent } from './user-search/user-search.component';
-import { PaginatorComponent } from './paginator/paginator.component';
 
 @NgModule({
   declarations: [
